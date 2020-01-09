400ceb8ab9e04ab6dc475f3dd5a856a070da8ab6
Franco Fusaro <ffusaro94@gmail.com>
2020-01-09 01:37:52 -0300

    Submitting first build
    First project commit.

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    int score = 0;
+    int currentWaveIndex = 0;
+    int currentWaveDeadEnemies = 0;
+    bool stopGame = false;
+
+    EnemySpawner enemySpawner;
+    void Awake()
+    {
+        SetUpSingleton();
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!stopGame)
+        {
+            if (enemySpawner.IsWaveFinished(currentWaveIndex, currentWaveDeadEnemies))
+            {
+                currentWaveIndex++;
+                currentWaveDeadEnemies = 0;
+                if (enemySpawner.HasMoreWaves(currentWaveIndex))
+                {
+                    StartCoroutine(enemySpawner.SpawnWave(currentWaveIndex));
+                }
+                else
+                {
+                    FindObjectOfType<SceneLoader>().LoadWin();
+                    stopGame = true;
+                }
+            }
+        }
+    }
+
+    public void AddDeadEnemy()
+    {
+        currentWaveDeadEnemies++;
+    }
+
+    public void AddScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 2f;
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+        GameSession gs = FindObjectOfType<GameSession>();
+        if (gs) { gs.ResetGame(); }
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(LoadGameOverCoroutine());
+    }
+
+    IEnumerator LoadGameOverCoroutine()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+    public void LoadWin()
+    {
+        StartCoroutine(LoadWinCoroutine());
+    }
+
+    IEnumerator LoadWinCoroutine()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene("WinScene");
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
